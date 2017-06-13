fcceb23ae5ad99fb6e4c1900ebd29d283562e76c
Alex Marchant <alexjmarchant@gmail.com>
2017-06-13 16:31:43 -0400

    Typo

--- server.rb
+++ server.rb
@@ -4,7 +4,7 @@ require_relative 'database'
 
 set :port, ENV['PORT']
 ROOT_URL = ENV['RACK_ENV'] == 'production' ?
-  ' https://todo-backend-sinatra-postgres.herokuapp.com' :
+  'https://todo-backend-sinatra-postgres.herokuapp.com' :
   'http://localhost:3000' 
 
 use TasksController
